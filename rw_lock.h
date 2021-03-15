d8bd6be686be177181de5758b688dfe78e92062b
yuank <yuank@qq.com>
2021-03-15 15:55:39 +0800

    write first lock update

--- rw_lock.h
+++ rw_lock.h
@@ -11,19 +11,19 @@ class rw_lock
 public:
 	rw_lock() = default;
 	~rw_lock() = default;
-public:
+
 	void lock_read()
 	{
 		std::unique_lock<std::mutex> ulk(counter_mutex);
-		cond_r.wait(ulk, [=]()->bool {return write_cnt == 0; });
+		cond_r.wait(ulk, [=]()->bool { return write_cnt == 0; });
 		++read_cnt;
 	}
 	void lock_write()
 	{
 		std::unique_lock<std::mutex> ulk(counter_mutex);
 		++write_cnt;
-		cond_w.wait(ulk, [=]()->bool {return read_cnt == 0 && !inwriteflag; });
-		inwriteflag = true;
+		cond_w.wait(ulk, [=]()->bool { return read_cnt == 0 && !is_writing_; });
+		is_writing_ = true;
 	}
 	void unlock_read()
 	{
@@ -44,13 +44,18 @@ public:
 		{
 			cond_w.notify_one();
 		}
-		inwriteflag = false;
+		is_writing_ = false;
 	}
+private:
+	rw_lock(const rw_lock&) = delete;
+	rw_lock(rw_lock&&) = delete;
+	rw_lock& operator=(const rw_lock&) = delete;
+	rw_lock& operator=(rw_lock&&) = delete;
 	
 private:
-	volatile size_t read_cnt{ 0 };
-	volatile size_t write_cnt{ 0 };
-	volatile bool inwriteflag{ false };
+	int read_cnt{ 0 };
+	int write_cnt{ 0 };
+	bool is_writing_{ false };
 	std::mutex counter_mutex;
 	std::condition_variable cond_w;
 	std::condition_variable cond_r;
@@ -60,41 +65,43 @@ template <typename _rw_lockable>
 class unique_wguard
 {
 public:
-	explicit unique_wguard(_rw_lockable &rw_lckable)
-		: rw_lckable_(rw_lckable)
+	explicit unique_wguard(_rw_lockable &rw_lck)
+		: rw_lck_(rw_lck)
 	{
-		rw_lckable_.lock_write();
+		rw_lck_.lock_write();
 	}
 	~unique_wguard()
 	{
-		rw_lckable_.unlock_write();
+		rw_lck_.unlock_write();
 	}
 private:
-	unique_wguard() = delete;
 	unique_wguard(const unique_wguard&) = delete;
+	unique_wguard(unique_wguard&&) = delete;
 	unique_wguard& operator=(const unique_wguard&) = delete;
+	unique_wguard& operator=(unique_wguard&&) = delete;
 private:
-	_rw_lockable &rw_lckable_;
+	_rw_lockable &rw_lck_;
 };
 template <typename _rw_lockable>
 class unique_rguard
 {
 public:
-	explicit unique_rguard(_rw_lockable &rw_lckable)
-		: rw_lckable_(rw_lckable)
+	explicit unique_rguard(_rw_lockable &rw_lck)
+		: rw_lck_(rw_lck)
 	{
-		rw_lckable_.lock_read();
+		rw_lck_.lock_read();
 	}
 	~unique_rguard()
 	{
-		rw_lckable_.unlock_read();
+		rw_lck_.unlock_read();
 	}
 private:
-	unique_rguard() = delete;
 	unique_rguard(const unique_rguard&) = delete;
+	unique_rguard(unique_rguard&&) = delete;
 	unique_rguard& operator=(const unique_rguard&) = delete;
+	unique_rguard& operator=(unique_rguard&&) = delete;
 private:
-	_rw_lockable &rw_lckable_;
+	_rw_lockable &rw_lck_;
 };
  
 #endif
